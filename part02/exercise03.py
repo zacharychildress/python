6b4899720458701d3729da4569802131925c9533
invadr <117381171+invadr@users.noreply.github.com>
2023-03-04 16:58:42 -0600

    solutions for part 2

---
+++ part02/exercise03.py
@@ -0,0 +1,5 @@
+# Write your solution here
+number = float(input("Please type in a number:"))
+decimal = number - int(number)
+print(f"Integer part: {int(number)}")
+print(f"Decimal part: {decimal}")
